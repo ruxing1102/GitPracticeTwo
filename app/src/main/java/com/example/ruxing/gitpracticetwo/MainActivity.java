25f22230ca833285f3a819dc16856f6d685f9adb
ruxing <ruxing@daodaoclub.com>
2018-05-27 13:57:14 +0800

    one分支第一次改动

--- app/src/main/java/com/example/ruxing/gitpracticetwo/MainActivity.java
+++ app/src/main/java/com/example/ruxing/gitpracticetwo/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //one分支的第一次改动
     }
 }
